105e0a1024a2ed9d96e252adf48979ec4657ea6d
Simone Manca <simone.manca.98@gmail.com>
2024-05-02 11:13:31 +0200

    table per bacheca e accentramenti dei p

--- src/components/Bacheca.jsx
+++ src/components/Bacheca.jsx
@@ -6,156 +6,131 @@ const Bacheca = () => {
   return (
     <div className="mt-0">
       <div
+        className="table-container"
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: "40px",
+          padding: "20px",
+          backgroundColor: "#F0F0F0",
+          borderRadius: "10px",
         }}
       >
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "auto 1fr",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <h4>Prezzi Latte</h4>
-            <Link to="/PrezziLatte">
-              <button
-                className="btn btn-primary rounded-circle"
-                style={{ padding: 0 }}
-              >
-                <img
-                  src="/latte.png"
-                  alt="Latte"
-                  width="50px"
-                  height="50px"
-                  style={{ borderRadius: "50%" }}
-                />
-              </button>
-            </Link>
-          </div>
-          <p className="text-no-underline centered-text">
-            Dettagli sui prezzi del latte disponibili qui.
-          </p>
-        </div>
+        <table style={{ width: "100%", borderSpacing: "0 20px" }}>
+          <tbody>
+            <tr>
+              <td style={{ textAlign: "center" }}>
+                <h4>Prezzi Latte</h4>
+                <Link to="/PrezziLatte">
+                  <button
+                    className="btn btn-primary rounded-circle"
+                    style={{ padding: 0 }}
+                  >
+                    <img
+                      src="/latte.png"
+                      alt="Latte"
+                      width="50px"
+                      height="50px"
+                      style={{ borderRadius: "50%" }}
+                    />
+                  </button>
+                </Link>
+              </td>
+              <td>
+                <p className="text-no-underline centered-text">
+                  Dettagli sui prezzi del latte disponibili qui.
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+                <hr />
+              </td>
+            </tr>
 
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "auto 1fr",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <h4>Prezzi Ovini</h4>
-            <Link to="/PrezziOvini">
-              <button
-                className="btn btn-primary rounded-circle"
-                style={{ padding: 0 }}
-              >
-                <img
-                  src="/ovini.png"
-                  alt="Ovini"
-                  width="50px"
-                  height="50px"
-                  style={{ borderRadius: "50%" }}
-                />
-              </button>
-            </Link>
-          </div>
-          <p className="text-no-underline centered-text">
-            Dettagli sui prezzi degli ovini disponibili qui.
-          </p>
-        </div>
+            <tr>
+              <td style={{ textAlign: "center" }}>
+                <h4>Prezzi Ovini</h4>
+                <Link to="/PrezziOvini">
+                  <button
+                    className="btn btn-primary rounded-circle"
+                    style={{ padding: 0 }}
+                  >
+                    <img
+                      src="/ovini.png"
+                      alt="Ovini"
+                      width="50px"
+                      height="50px"
+                      style={{ borderRadius: "50%" }}
+                    />
+                  </button>
+                </Link>
+              </td>
+              <td>
+                <p className="text-no-underline centered-text">
+                  Dettagli sui prezzi degli ovini disponibili qui.
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+                <hr />
+              </td>
+            </tr>
 
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "auto 1fr",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <h4>Prezzi Suini</h4>
-            <Link to="/PrezziSuini">
-              <button
-                className="btn btn-primary rounded-circle"
-                style={{ padding: 0 }}
-              >
-                <img
-                  src="/suini.png"
-                  alt="Suini"
-                  width="50px"
-                  height="50px"
-                  style={{ borderRadius: "50%" }}
-                />
-              </button>
-            </Link>
-          </div>
-          <p className="text-no-underline centered-text">
-            Dettagli sui prezzi dei suini disponibili qui.
-          </p>
-        </div>
+            <tr>
+              <td style={{ textAlign: "center" }}>
+                <h4>Prezzi Suini</h4>
+                <Link to="/PrezziSuini">
+                  <button
+                    className="btn btn-primary rounded-circle"
+                    style={{ padding: 0 }}
+                  >
+                    <img
+                      src="/suini.png"
+                      alt="Suini"
+                      width="50px"
+                      height="50px"
+                      style={{ borderRadius: "50%" }}
+                    />
+                  </button>
+                </Link>
+              </td>
+              <td>
+                <p className="text-no-underline centered-text">
+                  Dettagli sui prezzi dei suini disponibili qui.
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+                <hr />
+              </td>
+            </tr>
 
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "auto 1fr",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <h4>Prezzi Bovini</h4>
-            <Link to="/PrezziBovini">
-              <button
-                className="btn btn-primary rounded-circle"
-                style={{ padding: 0 }}
-              >
-                <img
-                  src="/bovini.png"
-                  alt="Bovini"
-                  width="50px"
-                  height="50px"
-                  style={{ borderRadius: "50%" }}
-                />
-              </button>
-            </Link>
-          </div>
-          <p className="text-no-underline centered-text">
-            Dettagli sui prezzi dei bovini disponibili qui.
-          </p>
-        </div>
+            <tr>
+              <td style={{ textAlign: "center" }}>
+                <h4>Prezzi Bovini</h4>
+                <Link to="/PrezziBovini">
+                  <button
+                    className="btn btn-primary rounded-circle"
+                    style={{ padding: 0 }}
+                  >
+                    <img
+                      src="/bovini.png"
+                      alt="Bovini"
+                      width="50px"
+                      height="50px"
+                      style={{ borderRadius: "50%" }}
+                    />
+                  </button>
+                </Link>
+              </td>
+              <td>
+                <p className="text-no-underline centered-text">
+                  Dettagli sui prezzi dei bovini disponibili qui.
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
