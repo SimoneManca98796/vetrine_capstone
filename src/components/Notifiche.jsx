dd1f9daa980bf9d300042bc2c9abb63c8eeebf6b
Simone Manca <simone.manca.98@gmail.com>
2024-06-14 15:51:46 +0200

    notifiche quasi finite

--- src/components/Notifiche.jsx
+++ src/components/Notifiche.jsx
@@ -19,25 +19,44 @@ const Notifiche = () => {
     <div className="notifiche-container container">
       <h1 className="notifiche-header">Notifiche</h1>
       <div className="notifiche-list">
-        {notifiche.map((notifica) => (
-          <div key={notifica.id} className="notifica-item">
-            <img
-              src={notifica.avatarURL || "/path/to/default/avatar.png"}
-              alt="User Avatar"
-              className="notifica-avatar"
-              onError={(e) => {
-                e.target.src = "/path/to/default/avatar.png";
-                console.error("Error loading avatar:", notifica.avatarURL);
-              }}
-            />
-            <div className="notifica-content">
-              <p>{notifica.messaggio}</p>
-              <a href={notifica.url} target="_blank" rel="noopener noreferrer">
-                Link
-              </a>
+        {notifiche.map((notifica) => {
+          const userName = notifica.fonte ? notifica.fonte : "Utente";
+          const dataOra = notifica.timestamp
+            ? new Date(notifica.timestamp).toLocaleString()
+            : "Data non disponibile";
+          return (
+            <div key={notifica.id} className="notifica-item">
+              <img
+                src={notifica.avatarURL || "/path/to/default/avatar.png"}
+                alt="User Avatar"
+                className="notifica-avatar"
+                onError={(e) => {
+                  e.target.src = "/path/to/default/avatar.png";
+                  console.error("Error loading avatar:", notifica.avatarURL);
+                }}
+              />
+              <div className="notifica-content">
+                <p>
+                  {notifica.messaggio}
+                  .
+                  <br />
+                  <strong>
+                    Dai un&#39;occhiata{" "}
+                    <a
+                      href={notifica.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      qui
+                    </a>
+                    .
+                  </strong>
+                </p>
+                <div className="notifica-timestamp">{dataOra}</div>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
