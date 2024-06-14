1b6cbd267169ed6d70d04dbc9d2a29469f74e676
Simone Manca <simone.manca.98@gmail.com>
2024-06-14 23:50:13 +0200

    adds

--- src/components/Notifiche.jsx
+++ src/components/Notifiche.jsx
@@ -28,45 +28,60 @@ const Notifiche = () => {
   return (
     <div className="notifiche-container container">
       <h1 className="notifiche-header">Notifiche</h1>
+      <div className="notifiche-didascalia">
+        <img
+          src="/bottomArrow.webp"
+          alt="Curious man"
+          className="notifiche-logo"
+        />
+        <span className="cartoon-font">Notifiche da visualizzare</span>
+      </div>
       <div className="notifiche-list">
-        {notifiche.map((notifica) => {
-          const userName = notifica.fonte ? notifica.fonte : "Utente";
-          const dataOra = notifica.timestamp
-            ? new Date(notifica.timestamp).toLocaleString()
-            : "Data non disponibile";
-          return (
-            <div key={notifica.id} className="notifica-item">
-              <img
-                src={notifica.avatarURL || "/path/to/default/avatar.png"}
-                alt="User Avatar"
-                className="notifica-avatar"
-                onError={(e) => {
-                  e.target.src = "/path/to/default/avatar.png";
-                  console.error("Error loading avatar:", notifica.avatarURL);
-                }}
-              />
-              <div className="notifica-content">
-                <p>
-                  {notifica.messaggio}
-                  .
-                  <br />
-                  <strong>
-                    Dai un&#39;occhiata{" "}
-                    <a
-                      href={notifica.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      qui
-                    </a>
+        {notifiche.length === 0 ? (
+          <div className="no-notifications">
+            <img src="/NoNotifica.webp" alt="No notifications" />
+            <p className="cartoon-font">Nessuna notifica da visualizzare!</p>
+          </div>
+        ) : (
+          notifiche.map((notifica) => {
+            const userName = notifica.fonte ? notifica.fonte : "Utente";
+            const dataOra = notifica.timestamp
+              ? new Date(notifica.timestamp).toLocaleString()
+              : "Data non disponibile";
+            return (
+              <div key={notifica.id} className="notifica-item">
+                <img
+                  src={notifica.avatarURL || "/path/to/default/avatar.png"}
+                  alt="User Avatar"
+                  className="notifica-avatar"
+                  onError={(e) => {
+                    e.target.src = "/path/to/default/avatar.png";
+                    console.error("Error loading avatar:", notifica.avatarURL);
+                  }}
+                />
+                <div className="notifica-content">
+                  <p>
+                    {notifica.messaggio}
                     .
-                  </strong>
-                </p>
-                <div className="notifica-timestamp">{dataOra}</div>
+                    <br />
+                    <strong>
+                      Dai un&#39;occhiata{" "}
+                      <a
+                        href={notifica.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        qui
+                      </a>
+                      .
+                    </strong>
+                  </p>
+                  <div className="notifica-timestamp">{dataOra}</div>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </div>
   );
