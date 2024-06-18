73dac2f0d499fbff6e69a27dff9ac9c4d8b3f1f5
Simone Manca <simone.manca.98@gmail.com>
2024-06-18 23:53:16 +0200

    ok

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     proxy: {
       "/api": {
@@ -21,4 +24,10 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  base: "/",
 });
