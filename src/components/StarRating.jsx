3f55017bd532b6b8dc7e683d04b2172c354a2d1b
Simone Manca <simone.manca.98@gmail.com>
2024-07-29 15:15:43 +0200

    ok

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,38 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
+import StarRatings from "react-star-ratings";
 import PropTypes from "prop-types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar as filledStar } from "@fortawesome/free-solid-svg-icons";
-import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
 import "../ProductReviews.css";
 
 const StarRating = ({ count, rating, onChangeRating }) => {
-  const [hoverRating, setHoverRating] = useState(0);
-
-  const handleClick = (index) => {
-    onChangeRating(index + 1);
-  };
-
-  const handleMouseOver = (index) => {
-    setHoverRating(index + 1);
-  };
-
-  const handleMouseOut = () => {
-    setHoverRating(0);
-  };
-
   return (
-    <div className="star-rating">
-      {Array.from({ length: count }, (_, index) => (
-        <FontAwesomeIcon
-          key={index}
-          icon={hoverRating > index || rating > index ? filledStar : emptyStar}
-          className={hoverRating > index || rating > index ? "selected" : ""}
-          onClick={() => handleClick(index)}
-          onMouseOver={() => handleMouseOver(index)}
-          onMouseOut={handleMouseOut}
-        />
-      ))}
-    </div>
+    <StarRatings
+      rating={rating}
+      starRatedColor="blue" // Cambia il colore degli stelli
+      changeRating={onChangeRating}
+      numberOfStars={count}
+      name="rating"
+      starDimension="24px" // Dimensione delle stelle
+      starSpacing="2px" // Spaziatura tra le stelle
+    />
   );
 };
 
